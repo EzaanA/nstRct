c80c346f617122020e09a49fd5099499225707f8
ezaan <ezaananjum1601@gmail.com>
2024-06-13 12:52:32 +0530

    first

---
+++ vdo1/app.js
@@ -0,0 +1,41 @@
+// const heading = React.createElement("h1",{},"HELLO WORLD FROM REACT!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+//   root.render(heading);
+
+/**
+ * 
+ * <div id="parent">
+ *   <div id="child">
+ *      <h1>I'm h1 tag</h1>
+ *      <h2>I'm h2 tag</h2>
+ *   </div>
+ *   <div id="child">
+ *      <h1>I'm h1 tag</h1>
+ *      <h2>I'm h2 tag</h2>
+ *   </div>
+ * </div>
+ * 
+ * 
+ * 
+ */
+
+
+const parent = React.createElement("div",{id:"child"},[
+  React.createElement("div",{id:"child"},
+    [
+      React.createElement("h1",{id:"h1d"},"I'm h1 tag"),
+      React.createElement("h2",{id:"h2d"},"I'm h2 tag"),
+    ]
+  ),
+
+  React.createElement("div",{id:"child"},
+    [
+      React.createElement("h1",{id:"h1d"},"I'm h1 tag"),
+      React.createElement("h2",{id:"h2d"},"I'm h2 tag"),
+    ]
+  )
+]
+)
+
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(parent);
