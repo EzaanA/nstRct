f8161f02c19433218f40a1ac2d2569742b9ea2e6
ezaan <ezaananjum1601@gmail.com>
2024-06-22 03:03:56 +0530

    ydaf

--- app.js
+++ app.js
@@ -19,13 +19,15 @@
  * 
  */
 
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div",{id:"child"},[
   React.createElement("div",{id:"child"},
     [
       React.createElement("h1",{id:"h1d"},"I'm h1 tag"),
       React.createElement("h2",{id:"h2d"},"I'm h2 tag"),
-    ]
+    ] 
   ),
 
   React.createElement("div",{id:"child"},
