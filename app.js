c3b52f3da9be01705a6ddaeec10ef13b80fbeb62
ezaan <ezaananjum1601@gmail.com>
2024-06-23 16:21:52 +0530

    doin

--- app.js
+++ app.js
@@ -22,22 +22,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{id:"child"},[
-  React.createElement("div",{id:"child"},
-    [
-      React.createElement("h1",{id:"h1d"},"I'm h1 tag"),
-      React.createElement("h2",{id:"h2d"},"I'm h2 tag"),
-    ] 
-  ),
-
-  React.createElement("div",{id:"child"},
-    [
-      React.createElement("h1",{id:"h1d"},"I'm h1 tag"),
-      React.createElement("h2",{id:"h2d"},"I'm h2 tag"),
-    ]
-  )
-]
-)
-
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(parent);
+const jsxHeading =(<h1>
+  Hi this is ezaan!
+  </h1>)
+const root =ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
