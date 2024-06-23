a0271619d4869e1453ff86bb9bef81fd6b38d062
ezaan <ezaananjum1601@gmail.com>
2024-06-23 17:29:01 +0530

    ComponentComposition

--- app.js
+++ app.js
@@ -18,12 +18,48 @@
  * 
  * 
  */
-
+// _________________________________________________
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
+//React Element
 const jsxHeading =(<h1>
-  Hi this is ezaan!
-  </h1>)
+  Hi this is React Element!
+  </h1>);
+
+  //React component with a return statement
+  const Heading1 =()=>{
+    return <h1>THis is heading inside of React component with a return statement</h1>
+  };
+
+  const Heading2=()=>(
+    <h1>THis is heading inside of React component without a return statement </h1>
+  )
+
+
+//converting the above react element to a react component
+const JsxHeadingAsComp = ()=>(
+  <div className="hello">
+    <h1>THis is React Element coverted to React Component</h1>
+  </div>
+);
+
+const ComponentComposition = ()=>(
+  <div className="he">
+    <h1>THis is React Component Composition:- </h1>
+    <Heading1/>
+  </div>
+);
+
+
+
 const root =ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+const root2 =ReactDOM.createRoot(document.getElementById("root2"));
+const root3 =ReactDOM.createRoot(document.getElementById("root3"));
+const root4 =ReactDOM.createRoot(document.getElementById("root4"));
+root.render(<JsxHeadingAsComp/>);
+root2.render(<Heading1/>);
+root3.render(<Heading2/>);
+root4.render(<ComponentComposition/>);
+// _________________________________________________________________
